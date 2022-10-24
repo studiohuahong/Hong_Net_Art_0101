6897aa02ac7ee77ecfd191585a1e0a9e50d87a84
Hong <studiohuahong@gmail.com>
2022-10-24 12:47:13 -0400

    Update

---
+++ week_seven/monster.js
@@ -0,0 +1,4 @@
+//reference: https://pixelencounter.com/Api/Monsters
+const mosterAPI = 'https://app.pixelencounter.com/api/basic/monsters/random/json';
+const monsterSVG = "view-source:https://app.pixelencounter.com/api/basic/monsters/random";
+
